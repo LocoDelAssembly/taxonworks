43cfa37aab269d526828dbaf8dac33196d2000c3
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-03-11 17:47:00 -0300

    Update 20130101000000_postgis_extend.rb

--- db/migrate/20130101000000_postgis_extend.rb
+++ db/migrate/20130101000000_postgis_extend.rb
@@ -1,7 +1,7 @@
 class PostgisExtend < ActiveRecord::Migration[4.2]
   def change
     # Deprecated.  This is not needed when rake db:setup is used
-    # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis')
-    # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis_topology')
+    ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis')
+    ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis_topology')
   end
 end
