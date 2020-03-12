19c4a1dab5cf05921217b01926731e0d58edbde3
Hernán Lucas Pereira <ellocodelassembler@gmail.com>
2020-03-12 15:52:01 -0300

    Update 20130101000000_postgis_extend.rb

--- db/migrate/20130101000000_postgis_extend.rb
+++ db/migrate/20130101000000_postgis_extend.rb
@@ -4,5 +4,6 @@ class PostgisExtend < ActiveRecord::Migration[4.2]
     # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis')
     # ActiveRecord::Base.connection.execute('CREATE EXTENSION postgis_topology')
     enable_extension "postgis" # https://github.com/rgeo/activerecord-postgis-adapter/issues/302#issuecomment-558717144
+    enable_extension "postgis_topology"
   end
 end
